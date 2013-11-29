b96dfb233ef2c59570d8f32e3961d1047dfdb44b
Josh Hill <jamesjoshuahill@gmail.com>
2013-11-29 12:15:31 +0000

    Add spec to check server's response body and code is passed on

--- proxy.rb
+++ proxy.rb
@@ -9,7 +9,13 @@ require 'net/http'
 set :port, ENV['PORT'] || 9494
 
 get '/*' do
-  forward = Net::HTTP::Get.new(request.url)
-  forward['X-API-Key'] = 'awesomeserver'
-  Net::HTTP.start('localhost', '4567') { |http| http.request(forward) }.body
+  proxy_request = Net::HTTP::Get.new(request.url)
+  proxy_request['X-API-Key'] = 'awesomeserver'
+
+  server_response = Net::HTTP.start('localhost', '4567') do |http|
+    http.request(proxy_request)
+  end
+  
+  status server_response.code
+  server_response.body
 end
--- spec/proxy_spec.rb
+++ spec/proxy_spec.rb
@@ -39,19 +39,31 @@ describe 'Proxy' do
 
   context 'with the X-API-Key header' do
 
-    it "adds the API key to the header" do
+    example "GET '/'" do
       server_request = stub_request(:get, 'http://localhost:4567').
                          with(:headers => { 'X-API-Key' => 'awesomeserver' })
       get_root
       expect(server_request).to have_been_requested
     end
 
-    it "returns the server's response" do
+  end
+
+  context "when returning the server's response it includes" do
+
+    example "body" do
       server_request = stub_request(:get, 'http://localhost:4567').
                          with(:headers => { 'X-API-Key' => 'awesomeserver' }).
                          to_return(:body => 'welcome')
       response = get_root
       expect(response.body).to eq 'welcome'
+    end
+
+    example "status code" do
+      server_request = stub_request(:get, 'http://localhost:4567').
+                         with(:headers => { 'X-API-Key' => 'awesomeserver' }).
+                         to_return(:status => 400)
+      response = get_root
+      expect(response.status).to eq 400
     end
 
   end
